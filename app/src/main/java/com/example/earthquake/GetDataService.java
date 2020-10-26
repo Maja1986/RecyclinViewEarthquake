3ecc231d2a2a0210c0a4c3542f907d69b25c35bb
PC <jankovskamaja@gmail.com>
2020-10-26 19:02:53 +0100

    EarthquakeRecyclerView

--- app/src/main/java/com/example/earthquake/GetDataService.java
+++ app/src/main/java/com/example/earthquake/GetDataService.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface GetDataService {
-    @GET("/earthquakes")
+    @GET("/https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&limit=20z")
     Call<List<Earthquake>> getAllEarthquakes();
 }
